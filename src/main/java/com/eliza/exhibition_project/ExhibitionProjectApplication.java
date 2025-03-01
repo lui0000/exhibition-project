b6f0c96aed1dcd15bcbcee22d553dc3bc2b196a8
Lisa <elbondarenko04@gmail.com>
2025-03-01 18:48:04 +0300

    first commit

---
+++ src/main/java/com/eliza/exhibition_project/ExhibitionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eliza.exhibition_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliza/exhibition_project/ExhibitionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliza.exhibition_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
